1fd6a9e76ae6d19a71054c9f6bfd4155367d6e45
yuunseo <dbstj7457@naver.com>
2023-02-21 12:31:10 +0900

    페이지 전환

--- src/index.js
+++ src/index.js
@@ -1,13 +1,68 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter,Route,Routes} from "react-router-dom";
+
+function NotFound(){
+  return(
+    <div>
+      Not Found :^(
+    </div>
+  )
+}
+function Home(){
+  return(
+    <div>
+      <h2>Home</h2>
+      Home...
+    </div>
+  )
+}
+
+function Topic(){
+  return(
+    <div>
+      <h2>Topic</h2>
+      Topic...
+    </div>
+  )
+}
+
+function Contact(){
+  return(
+    <div>
+      <h2>Contact</h2>
+      Contact...
+    </div>
+  )
+}
+
+function App(){
+  return(
+    <div>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/topics">Topics</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+      <h1>React Router DOM example</h1>
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+        <Route path="/topics" element={<Topic />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/*" element={<NotFound/>}></Route>
+      </Routes>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
